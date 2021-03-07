9ebbd694007d40dead835363ca79e12675f04541
Faiq <faiq.kaboel@gmail.com>
2021-03-07 12:38:16 +0700

    assets minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,26 @@
 const path = require("path");
-const config = require("./webpack.config");
+const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(config, {
+module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "static/js/[name].[contenthash].bundle.js",
     path: path.resolve(process.cwd(), "dist"),
+    clean: true,
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 });
