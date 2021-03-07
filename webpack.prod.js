24ee7b594d63ad17e31eedb935bd46595c58ed9a
Faiq <faiq.kaboel@gmail.com>
2021-03-07 09:52:09 +0700

    setup development and production environment

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
